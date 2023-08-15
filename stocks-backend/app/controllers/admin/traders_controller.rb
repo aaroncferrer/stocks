beb1511cb38e12eb5c6157ee85a7351424f2c74f
Anthony <arcferrer5@gmail.com>
2023-08-15 20:38:56 +0800

    Generate controllers for Admin::Traders

---
+++ stocks-backend/app/controllers/admin/traders_controller.rb
@@ -0,0 +1,50 @@
+require 'jwt_auth'
+
+class Admin::TradersController < ApplicationController
+    before_action :require_admin
+
+    def index
+        traders = Trader.all
+        render json: traders
+    end
+
+    def show
+        trader = Trader.find(params[:id])
+        render json: trader
+    end
+
+    def create
+        trader = Trader.new(trader_params)
+
+        if trader.status == "approved"
+            trader.confirmed_at = Time.current
+        end
+
+        if trader.save
+            render json: trader, status: :created
+        else
+            render json: { errors: trader.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        trader = Trader.find(params[:id])
+        if trader.update(trader_params)
+            render json: trader
+        else 
+            render json: { errors: trader.errors.full_messages }, status: :unprocessable_entity
+        end
+    end 
+
+    private
+
+    def trader_params
+        params.require(:trader).permit(:first_name, :last_name, :email, :password, :password_confirmation, :status)
+    end
+
+    def require_admin
+        unless @current_user.is_a?(Admin)
+            render json: { error: 'Only admin users are allowed to perform this action' }, status: :forbidden
+        end
+    end
+end
--- stocks-backend/config/routes.rb
+++ stocks-backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   post '/admin/signup', to: 'admin_auth#signup'
   post '/admin/login', to: 'admin_auth#login'
+
+  namespace :admin do
+    resources :traders, only: [:index, :show, :create, :update]
+  end
 end
