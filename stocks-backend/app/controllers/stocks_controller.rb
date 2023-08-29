e3fadd864ff53d318fc1e0814409dbdfc811fd1d
Anthony <arcferrer5@gmail.com>
2023-08-29 21:05:47 +0800

    Remove auth for refreshing stocks

--- stocks-backend/app/controllers/stocks_controller.rb
+++ stocks-backend/app/controllers/stocks_controller.rb
@@ -3,6 +3,8 @@ Rails.application.load_tasks
 require 'jwt_auth'
 
 class StocksController < ApplicationController
+  skip_before_action :check_auth, only: [:refresh]
+
   def index
     stocks = Stock.order(:id)
     render json: stocks
